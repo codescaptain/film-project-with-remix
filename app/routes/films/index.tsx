48e22fe561a4961cd730ffdd181873faf2feefee
codescaptain <codescaptain@gmail.com>
2022-09-17 13:04:32 +0300

    [add] film searching feature

--- app/routes/films/index.tsx
+++ app/routes/films/index.tsx
@@ -1,11 +1,13 @@
 
 import type { LoaderFunction, MetaFunction } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
+import { Form, useLoaderData } from "@remix-run/react";
 import { Film, getFilms } from "~/api/films";
 
 // SERVER SIDE
-export const loader: LoaderFunction = async () => {
-return getFilms();
+export const loader: LoaderFunction = async ({request}) => {
+    const url = new URL(request.url)
+    const title = url.searchParams.get('title')
+    return getFilms(title);
 }
 
 export const meta: MetaFunction = () => ({
@@ -22,6 +24,25 @@ export default function FilmsIndex(){
     return (
     <div className="p-16 font-sans">
         <h1 className="text-6xl font-bold text-center">Studio Ghibli Films</h1>
+        
+        <Form reloadDocument method="get" className="py-5">
+            <label className="font-bold">
+                Search {' '}
+                <input 
+                    type="text"
+                    name="title"
+                    placeholder="Type a title.."
+                    className="border-2 rounded py-2 px-3" 
+                />
+             </label>
+
+             <button 
+                type="submit" 
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-2"> 
+                Search 
+             </button>
+        </Form>
+
         <div className="grid grid-cols-2 lg:grid-cols-4 gap-4">
             {films.map((film) => (
                 <div key={film.title} className="hover:shadow-2xl hover:scale-105 hover:font-bold cursor-pointer">
